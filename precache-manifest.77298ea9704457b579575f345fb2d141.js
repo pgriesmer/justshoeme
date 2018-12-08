bd59166537d22a59b7cb0ae9e5fcdb347cf28078
Peter L Griesmer <pgriesmer@peters-air.devices.brown.edu>
2018-12-07 19:19:26 -0500

    Updates

---
+++ precache-manifest.77298ea9704457b579575f345fb2d141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff48125222da3fb2d39a",
+    "url": "/justshoeme/static/js/runtime~main.ff481252.js"
+  },
+  {
+    "revision": "ce71bc917e1316030866",
+    "url": "/justshoeme/static/js/main.ce71bc91.chunk.js"
+  },
+  {
+    "revision": "ed3bf9a6a0312ccbe475",
+    "url": "/justshoeme/static/js/1.ed3bf9a6.chunk.js"
+  },
+  {
+    "revision": "ce71bc917e1316030866",
+    "url": "/justshoeme/static/css/main.3339473e.chunk.css"
+  },
+  {
+    "revision": "a56a5250d6eec65e6c2c01b63a4317aa",
+    "url": "/justshoeme/index.html"
+  }
+];
